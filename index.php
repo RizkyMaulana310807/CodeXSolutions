f97373e94f6608eb04ab10f1cd1fba26ba3d029b
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2024-11-06 18:52:00 +0700

    First Init

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/ico/favicon.ico" type="image/x-icon">
+    <title>CodeX Solutions</title>
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="./css/output.css">
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <nav class="bg-red-500">
+        <!-- logo -->
+        <div>
+            
+        </div>
+
+        <!-- menu -->
+         <div></div>
+
+         <!-- button -->
+          <div></div>
+        
+    </nav>
+
+
+</body>
+
+</html>
