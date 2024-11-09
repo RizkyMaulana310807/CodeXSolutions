350c15d7b6c0196b0eadd104767a6efda1ed65c8
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2024-11-09 08:20:49 +0700

    Mobile responsive

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
     <nav class="bg-yl py-4 flex flex-row justify-between px-4 items-center drop-shadow-lg">
         <!-- logo -->
         <div class="flex flex-row items-center gap-2 hover:cursor-pointer hover:translate-y-[10px] transition-all ease-in-out duration-200 group">
-            <img class="md:h-9 md:w-9 xs:w-6 xs:h-6 rounded-full group-hover:-translate-y-[10px] transition-all ease-in-out duration-200" src="img/logo/logo.png" alt="logo.png">
-            <h1 class="xs:text-xs xl:text-lg font-semibold bg-bl rounded-lg px-4 py-2 shadow-keycaps-bl border-2 border-gray-800 group-hover:shadow-active-bl transition-all ease-in-out duration-200"><span class="text-gray-900 font-bold text-xl">CodeX</span> Solutions</h1>
+            <img class="md:h-9 md:w-9 xxs:w-6 xxs:h-6 rounded-full group-hover:-translate-y-[10px] transition-all ease-in-out duration-200" src="img/logo/logo.png" alt="logo.png">
+            <h1 class="xxs:text-xs xl:text-lg font-semibold bg-bl rounded-lg xxs:px-2 xxs:py-1 md:px-4 md:py-2 shadow-keycaps-bl border-2 border-gray-800 group-hover:shadow-active-bl transition-all ease-in-out duration-200"><span class="text-gray-900 font-bold xxs:text-sm md:text-xl">CodeX</span> Solutions</h1>
         </div>
 
         <!-- menu -->
@@ -123,9 +123,9 @@
 
         <div class="flex flex-row items-center space-x-4 px-4 bg-yl">
             <h1 class="xxs:text-xs md:text-xl font-semibold text-gray-700 hover:text-white transition-all ease-in-out duration-200">Supported Payment Method : </h1>
-            <img src="img/logo/dana.png" alt="dana.png" class="md:w-32 md:h-32 xxs:h-14 xxs:w-h-14 object-contain filter grayscale hover:grayscale-0 transition duration-300">
-            <img src="img/logo/ovo.png" alt="ovo.png" class="md:w-32 md:h-32 xxs:h-14 xxs:w-h-14 object-contain filter grayscale hover:grayscale-0 transition duration-300">
-            <img src="img/logo/paypal.png" alt="paypal.png" class="md:w-32 md:h-32 xxs:h-14 xxs:w-h-14 object-contain filter grayscale hover:grayscale-0 transition duration-300">
+            <img src="img/logo/dana.png" alt="dana.png" class="xxs:h-14 xxs:w-14 md:w-32 md:h-32 object-contain filter grayscale hover:grayscale-0 transition duration-300">
+            <img src="img/logo/ovo.png" alt="ovo.png" class="xxs:h-14 xxs:w-14 md:w-32 md:h-32 object-contain filter grayscale hover:grayscale-0 transition duration-300">
+            <img src="img/logo/paypal.png" alt="paypal.png" class="xxs:h-14 xxs:w-14 md:w-32 md:h-32 object-contain filter grayscale hover:grayscale-0 transition duration-300">
         </div>
 
         <div class="flex flex-col justify-center items-center gap-4 p-6">
@@ -160,27 +160,27 @@
 
         <div class="flex flex-row ">
 
-            <div class="xs:hidden xs:w-full md:flex xl:w-1/2 overflow-hidden bg-yl">
+            <div class="xxs:hidden xs:w-full md:flex xl:w-1/2 overflow-hidden bg-yl">
                 <img class="object-cover transition-transform duration-300" src="img/illustration/persons_digital.png" alt="persos.png">
             </div>
 
 
-            <div class="xl:flex xl:w-1/2 xl:flex-col bg-yl xl:p-6 md:justify-around">
+            <div class="xxs:flex xxs:flex-col xxs:w-full xxs:h-96 xxs:justify-between xl:flex xl:w-1/2 xl:flex-col bg-yl xl:p-6 md:justify-around">
 
-                <h1 class="text-2xl font-bold font-bona text-gray-700">Kenapa <span class="text-gray-900 bg-pk border-2 border-gray-700 shadow-keycaps-pk rounded-lg px-2 py-1">CodeX</span>?</h1>
+                <h1 class="xxs:text-sm text-2xl font-bold font-bona text-gray-700">Kenapa <span class="text-gray-900 bg-pk border-2 border-gray-700 shadow-keycaps-pk rounded-lg px-2 py-1">CodeX</span>?</h1>
 
                 <div class="flex justify-evenly gap-4">
-                    <div class="bg-[#ABDAFC] drop-shadow-md inline-flex w-40 h-40 p-6 flex-col text-center rounded-lg border-2 border-gray-700 shadow-keycaps-bl hover:shadow-active-bl hover:translate-y-[10px] transition-all ease-in-out">
+                    <div class="bg-[#ABDAFC] drop-shadow-md inline-flex xxs:w-32 xxs:h-40 md:w-40 md:h-40 p-6 flex-col text-center rounded-lg border-2 border-gray-700 shadow-keycaps-bl hover:shadow-active-bl hover:translate-y-[10px] transition-all ease-in-out">
                         <h1 class="underline underline-offset-4 font-bold text-slate-900">200+</h1>
-                        <p class="font-light text-slate-900">Kami sudah melayani lebih dari 200+ Client</p>
+                        <p class="xxs:text-xs font-light text-slate-900">Kami sudah melayani lebih dari 200+ Client</p>
                     </div>
-                    <div class="bg-[#F88DAD] drop-shadow-md inline-flex w-40 h-40 p-6 flex-col text-center rounded-lg border-2 border-gray-700 shadow-keycaps-pk hover:shadow-active-pk hover:translate-y-[10px] transition-all ease-in-out">
+                    <div class="bg-[#F88DAD] drop-shadow-md inline-flex xxs:w-32 xxs:h-40 md:w-40 md:h-40 p-6 flex-col text-center rounded-lg border-2 border-gray-700 shadow-keycaps-pk hover:shadow-active-pk hover:translate-y-[10px] transition-all ease-in-out">
                         <h1 class="underline underline-offset-4 font-bold text-slate-900">75+</h1>
-                        <p class="font-light text-slate-900">Kami telah menyelesaikan lebih dari 75+ Project</p>
+                        <p class="xxs:text-xs font-light text-slate-900">Kami telah menyelesaikan lebih dari 75+ Project</p>
                     </div>
-                    <div class="bg-[#FAC748] drop-shadow-md inline-flex w-40 h-40 p-6 flex-col text-center rounded-lg border-2 border-gray-700 shadow-keycaps-yl hover:shadow-active-yl hover:translate-y-[10px] transition-all ease-in-out">
+                    <div class="bg-[#FAC748] drop-shadow-md inline-flex xxs:w-32 xxs:h-40 md:w-40 md:h-40 p-6 flex-col text-center rounded-lg border-2 border-gray-700 shadow-keycaps-yl hover:shadow-active-yl hover:translate-y-[10px] transition-all ease-in-out">
                         <h1 class="underline underline-offset-4 font-bold text-slate-900">175+</h1>
-                        <p class="font-light text-slate-900">Kami telah mendapat 275 Review Positive</p>
+                        <p class="xxs:text-xs font-light text-slate-900">Kami telah mendapat 275 Review Positive</p>
                     </div>
                 </div>
 
